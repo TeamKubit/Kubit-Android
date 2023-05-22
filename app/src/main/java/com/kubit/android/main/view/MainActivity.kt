826e369db6aefe799fe7241b9500be1680de1b5f
ByeongSu Kim <kimbsu3000@gmail.com>
2023-05-22 23:56:15 +0900

    feat: 메인 화면 구현 시작

--- app/src/main/java/com/kubit/android/main/view/MainActivity.kt
+++ app/src/main/java/com/kubit/android/main/view/MainActivity.kt
@@ -1,12 +1,58 @@
 package com.kubit.android.main.view
 
 import android.os.Bundle
-import com.kubit.android.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.kubit.android.base.BaseActivity
+import com.kubit.android.base.BaseViewModel
+import com.kubit.android.databinding.ActivityMainBinding
+import com.kubit.android.main.viewmodel.MainViewModel
 
 class MainActivity : BaseActivity() {
+
+    private val model: MainViewModel by lazy {
+        ViewModelProvider(
+            this,
+            BaseViewModel.Factory(application)
+        )[MainViewModel::class.java]
+    }
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    // region Activity LifeCycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setObserver()
+        init()
+    }
+    // endregion Activity LifeCycle
+
+    private fun setObserver() {
+        model.progressFlag.observe(this, Observer { progressFlag ->
+            if (progressFlag) {
+                showProgress()
+            } else {
+                dismissProgress()
+            }
+        })
+
+        model.apiFailMsg.observe(this, Observer { failMsg ->
+            if (failMsg.isNotEmpty()) {
+                model.setProgressFlag(false)
+                showToastMsg(failMsg)
+            }
+        })
+
+        model.exceptionData.observe(this, Observer { exception ->
+            model.setProgressFlag(false)
+            showErrorMsg()
+        })
+    }
+
+    private fun init() {
+
     }
 }
--- app/src/main/java/com/kubit/android/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/kubit/android/main/viewmodel/MainViewModel.kt
@@ -1,10 +1,24 @@
 package com.kubit.android.main.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.kubit.android.base.BaseViewModel
+import com.kubit.android.model.data.route.KubitTabRouter
 
 class MainViewModel : BaseViewModel() {
 
+    /**
+     * 현재 선택된 탭이 어떤 탭인지
+     */
+    private val _tabRouter: MutableLiveData<KubitTabRouter> =
+        MutableLiveData(KubitTabRouter.COIN_LIST)
+    val tabRouter: LiveData<KubitTabRouter> get() = _tabRouter
 
+    fun setTabRouter(pTabRouter: KubitTabRouter) {
+        if (tabRouter.value != pTabRouter) {
+            _tabRouter.value = pTabRouter
+        }
+    }
 
     companion object {
         private const val TAG: String = "MainViewModel"
