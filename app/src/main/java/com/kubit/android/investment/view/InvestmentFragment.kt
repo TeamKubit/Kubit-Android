bee418dc4bb63e4d763715d8f2e49b3195dca41c
ByeongSu Kim <kimbsu3000@gmail.com>
2023-05-24 01:02:08 +0900

    feat: 투자내역 화면 구현

--- app/src/main/java/com/kubit/android/investment/view/InvestmentFragment.kt
+++ app/src/main/java/com/kubit/android/investment/view/InvestmentFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.tabs.TabLayout
 import com.kubit.android.R
 import com.kubit.android.base.BaseFragment
 import com.kubit.android.databinding.FragmentInvestmentBinding
@@ -41,7 +42,46 @@ class InvestmentFragment : BaseFragment() {
     // endregion Fragment LifeCycle
 
     private fun init() {
+        binding.apply {
+            tlInvestment.apply {
+                addTab(
+                    newTab().setId(R.id.investment_tab_asset)
+                        .setText(getString(R.string.investment_tab_asset))
+                )
+                addTab(
+                    newTab().setId(R.id.investment_tab_record)
+                        .setText(getString(R.string.investment_tab_record))
+                )
+                addTab(
+                    newTab().setId(R.id.investment_tab_not_yet)
+                        .setText(getString(R.string.investment_tab_not_yet))
+                )
 
+                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+                        when (tab?.id) {
+                            R.id.investment_tab_asset -> {
+                                tvInvestmentDeleteSelectOrder.visibility = View.GONE
+                            }
+
+                            R.id.investment_tab_record -> {
+                                tvInvestmentDeleteSelectOrder.visibility = View.GONE
+                            }
+
+                            R.id.investment_tab_not_yet -> {
+                                tvInvestmentDeleteSelectOrder.visibility = View.VISIBLE
+                            }
+                        }
+                    }
+
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+                    }
+
+                    override fun onTabReselected(tab: TabLayout.Tab?) {
+                    }
+                })
+            }
+        }
     }
 
     companion object {
